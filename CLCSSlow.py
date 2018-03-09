d3306f9a673fdad5786355e671a11a05738d5178
David Mora <davidnmora@gmail.com>
2018-03-08 22:29:41 -0800

    Complete part f

--- CLCSSlow.py
+++ CLCSSlow.py
@@ -3,14 +3,21 @@ import numpy as np
 from cut import cut
 from LCS import LCS
 
+def CLCS(A,B):
+	bestLCS = 0
+	for i in range(len(A)):
+		currLCS = LCS(cut(A, i), B)
+		if currLCS > bestLCS:
+			bestLCS = currLCS
+	return bestLCS
+
 def main():
-	print "CUT TEST: " + cut("yayPUTMEFIRST", 3)
 	if len(sys.argv) != 1:
 		sys.exit('Usage: `python LCS.py < input`')
 	
-	for l in sys.stdin:
-		A,B = l.split()
-		print LCS(A,B)
+	for line in sys.stdin:
+		A,B = line.split()
+		print "SOLUTION: " + str(CLCS(A,B))
 	return
 
 if __name__ == '__main__':
