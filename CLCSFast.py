bcb211e70ae0cab78748b2b899a2aff51a92b280
David Mora <davidnmora@gmail.com>
2018-03-10 22:39:59 -0800

    Create shell of program (uses unbounded LCS)

--- CLCSFast.py
+++ CLCSFast.py
@@ -1,9 +1,8 @@
 import sys
 import numpy as np
 
-arr = np.zeros((2048, 2048), dtype=int)
-
-def LCS(A,B):
+def singleShortestPath(A,B, pathStartIndex, lowerBoundPath, upperBoundPath):
+	arr = np.zeros((2048, 2048), dtype=int)
 	m = len(A)
 	n = len(B)
 
@@ -14,15 +13,34 @@ def LCS(A,B):
 			else:
 				arr[i][j] = max(arr[i-1][j], arr[i][j-1])
 
+	# TO DO: store upper and lower path in their respective matrixes
 	return arr[m][n]
+
+def findShortestPath(A, B, p, l, u):
+	print p
+	if u - l <= 1:
+		return
+	mid = (l + u) / 2
+	print "mid: %d" % mid
+	p[mid] = singleShortestPath(A, B, mid, p[l], p[u])
+	findShortestPath(A, B, p, l, mid)
+	findShortestPath(A, B, p, mid, u)
 
 def main():
 	if len(sys.argv) != 1:
-		sys.exit('Usage: `python LCS.py < input`')
+		sys.exit('Usage: `python CLCSFast.py < input`')
 	
 	for l in sys.stdin:
 		A,B = l.split()
-		print LCS(A,B)
+		p = np.zeros(len(A), dtype=int)
+		l = 0
+		u = len(A) - 1
+		# find initial bounding path
+		zeros = np.zeros(len(B), dtype=int)
+		uppers = np.full(2, len(A), dtype=int)
+		p[0] = singleShortestPath(A, B, l, zeros, uppers)
+		p[u] = singleShortestPath(A, B, u, zeros, uppers)
+		findShortestPath(A, B, p, l, u) # TO DO: return shortest path
 	return
 
 if __name__ == '__main__':
