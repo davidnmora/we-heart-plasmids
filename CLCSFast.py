0d3fdb8ad6b62e83e37d6d2148f4f0a730efd186
David Mora <davidnmora@gmail.com>
2018-03-11 18:44:55 -0700

    Implement backtrace, but with off by one bugs etc

--- CLCSFast.py
+++ CLCSFast.py
@@ -1,24 +1,73 @@
 import sys
 import numpy as np
 
-MATRIX_SIZE = 2048
+MATRIX_SIZE = 6
 
 lowerMatrix = np.zeros((MATRIX_SIZE, MATRIX_SIZE), dtype=int)
 upperMatrix = np.zeros((MATRIX_SIZE, MATRIX_SIZE), dtype=int)
 dpTable = np.zeros((2*MATRIX_SIZE, MATRIX_SIZE), dtype=int)
+
+def isInBounds(row, col, lowerBoundPath, upperBoundPath):
+	return (row >= upperBoundPath[col]) and (row <= lowerBoundPath[col])
+
+def backtrackAndStorePath(B, pathStartIndex, l, u):
+	# start at end, insert into lowerMatrix and upperMatrix as you go
+	row  = pathStartIndex
+
+	for col in range(len(B), 1, -1):
+		# TO DO: check bounds
+		# update variables
+		top  = dpTable[row-1][col]
+		left = dpTable[row]  [col-1]
+		diag = dpTable[row-1][col-1]
+		curr = dpTable[row]  [col]
+		
+		# UPDATE lower and upper matrixes
+		lowerMatrix[pathStartIndex][col] = row
+		upperMatrix[pathStartIndex][col] = row # can be overwritten by vertical movements
+
+		if diag+1 == curr:
+			# take diagonal
+			print("In diagonal case")
+			row -= 1
+		elif top > left:
+			# take top until we take left or diag
+			while True:
+				row -= 1
+				curr = dpTable[row][col]
+				top  = dpTable[row-1][col]
+				left = dpTable[row][col-1]
+				diag = dpTable[row][col-1]
+				if (diag+1 == curr) or (left > top):
+					break
+			# Update the upperMatrix with the "highest" (smallest) number after moving up vertically
+			upperMatrix[pathStartIndex][col] = row
+		print("row: ", row)
+		# else take left
+
+		# Update the lower and upperMatrix at column index 0 with pathStartIndex
+		lowerMatrix[pathStartIndex][0] = pathStartIndex
+		upperMatrix[pathStartIndex][0] = pathStartIndex
+	return
+
+
 #SingleShortestPath: updates the DP table for the values within the bounds, and then
 #backtrack to get fill in the upper and lower paths for the matrices
-def singleShortestPath(A,B, pathStartIndex, lowerBoundPath, upperBoundPath):
+def singleShortestPath(A,B, pathStartIndex, l, u):
 	m = len(A)
 	n = len(B)
 	# 1. FILL IN DP TABLE (within bounding constraints)
-	for i in range(1,m+1):
-		for j in range(1,n+1):
-			if A[i-1] == B[j-1]:
-				dpTable[i][j] = dpTable[i-1][j-1]+1
-			else:
-				dpTable[i][j] = max(dpTable[i-1][j], dpTable[i][j-1])
-	# 2. BACKTRACCE TO STORE SHORTEST PATH IN lowerMatrix and upperMatrix
+	for row in range(1, m+1):
+		for col in range(1,n+1):
+			print(row, col)
+			if isInBounds(row, col, lowerMatrix[l], upperMatrix[u]):
+				if A[row-1] == B[col-1]:
+					dpTable[row+pathStartIndex][col] = dpTable[pathStartIndex+row-1][col-1]+1
+				else:
+					dpTable[pathStartIndex+row][col] = max(dpTable[pathStartIndex+row-1][col], dpTable[pathStartIndex+row][col-1])
+			# 2. BACKTRACCE TO STORE SHORTEST PATH IN lowerMatrix and upperMatrix
+	print dpTable
+	backtrackAndStorePath(B, pathStartIndex, l, u)
 	return
 
 def findShortestPath(A, B, lowerMatrix, upperMatrix, l, u):
@@ -27,7 +76,7 @@ def findShortestPath(A, B, lowerMatrix, upperMatrix, l, u):
 		return
 	mid = (l + u) / 2
 	print "mid: %d" % mid
-	singleShortestPath(A, B, mid, lowerMatrix[l], upperMatrix[u])
+	singleShortestPath(A, B, mid, l, u)
 	findShortestPath(A, B, lowerMatrix, upperMatrix, l, mid)
 	findShortestPath(A, B, lowerMatrix, upperMatrix, mid, u)
 	return
@@ -39,11 +88,11 @@ def main():
 	for l in sys.stdin:
 		A,B = l.split()
 		p = np.zeros(len(A), dtype=int)
-		l = len(A) - 1
+		l = len(A)
 		u = 0
 		# SETUP: find initial bounding path before running recursive algorithm
-		singleShortestPath(A, B, l, lowerMatrix[l], upperMatrix[u])
-		singleShortestPath(A, B, u, lowerMatrix[l], upperMatrix[u])
+		singleShortestPath(A, B, l, l, u)
+		singleShortestPath(A, B, u, l, u)
 
 		# THE REAL DEAL: run the algorithm
 		findShortestPath(A, B, lowerMatrix, upperMatrix, l, u) # TO DO: return shortest path
